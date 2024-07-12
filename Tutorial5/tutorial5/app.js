a957035349da6292429e8bc44e99ebe354f6fdc5
keyush patel <ky280311@dal.ca>
2024-07-11 21:28:17 -0300

    tutorial 5

--- Tutorial5/tutorial5/app.js
+++ Tutorial5/tutorial5/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port =  process.env.PORT || 3000;
 let id=1;
 
 let users = [
